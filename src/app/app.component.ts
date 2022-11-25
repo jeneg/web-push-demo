5b0f26e5ff1dd9e3b183d67ed715366d5a74a5d1
jeneg <polizey565@gmail.com>
2022-11-25 15:34:53 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwPush} from "@angular/service-worker";
+import {HttpClient} from "@angular/common/http";
+import {lastValueFrom, tap} from "rxjs";
+
+const PUBLIC_VAPID_KEY_OF_SERVER = {
+  "publicKey": "BKMBshDExl6CQiJLloq5d_w7F67jCwh_p84kP07U3OEUxRpjIJEbfTEXZdCcBZfibGBvkm1i5S4m5eckrXl33mk",
+  "privateKey": "LjxT-QJI8V3X0cjokh8qelmGJ3U1_Mu-hBjcdEYlWF8"
+}
+
+interface SubscriptionItem {
+  info: string;
+  subscription: PushSubscription;
+  id: number
+}
+
+// const apiPath = 'http://localhost:4000'
+const apiPath = 'https://web-push-demo-api.onrender.com'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web-push';
+  message = '';
+
+  allSubscriptions: SubscriptionItem[] = [];
+
+  constructor(public swPush: SwPush, private httpClient: HttpClient) {
+  }
+
+  async ngOnInit() {
+    await this.loadSubscriptions();
+
+    console.log('subscription', this.swPush.subscription);
+
+    this.swPush.messages.subscribe(res => console.log('push message: ', res))
+  }
+
+  subscribe() {
+    this.subscribeToPush();
+  }
+
+  async push(item: SubscriptionItem) {
+    const SERVER_URL = `${apiPath}/api/send-notification`;
+    const response = await fetch(SERVER_URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({id: item.id, message: this.message})
+    });
+
+    this.message = '';
+  }
+
+  private async subscribeToPush() {
+    try {
+      const sub = await this.swPush.requestSubscription({
+        serverPublicKey: PUBLIC_VAPID_KEY_OF_SERVER.publicKey,
+      });
+
+      console.log(sub);
+
+      await this.saveSubscription(sub)
+      await this.loadSubscriptions();
+
+      console.log(this.allSubscriptions);
+    } catch (err) {
+      console.error('Could not subscribe due to:', err);
+    }
+  }
+
+  async saveSubscription(subscription: PushSubscription)  {
+    const SERVER_URL = `${apiPath}/api/save-subscription`;
+    const response = await fetch(SERVER_URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({subscription, info: navigator.userAgent})
+    });
+
+    return response.json();
+  };
+
+  async loadSubscriptions() {
+    return lastValueFrom(this.httpClient.get<SubscriptionItem[]>(`${apiPath}/api/subscriptions`)).then(res => {
+      this.allSubscriptions = res
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      // enabled: !isDevMode(),
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
